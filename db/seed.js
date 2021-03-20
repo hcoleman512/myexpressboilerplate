7e75b0ecb3022ee6e169441b63a11e7df528d273
Henry Coleman <henrycoleman@henrys-mbp.lan>
2021-03-20 11:01:45 -0500

    first commit

---
+++ db/seed.js
@@ -0,0 +1,20 @@
+//Import The Database Connection
+const mongoose = require("./connection")
+
+///////////////////////////////////////////
+// IMPORT YOUR MODELS BELOW
+///////////////////////////////////////////
+
+///////////////////////////////////////////
+// DO YOUR DATABASE OPERATIONS IN BELOW FUNCTION
+///////////////////////////////////////////
+
+const seed = async () => {
+  //*********Code Goes Here
+
+  console.log("seed file")
+  //***************************** */
+}
+
+// Run Seed Function
+seed()
